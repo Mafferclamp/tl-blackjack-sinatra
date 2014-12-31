7c535978f5fa4b201ac8064ad236d63db36e7170
Arnold Lei <arnold.lei88@gmail.com>
2014-12-31 00:51:33 -0500

    running to a no method 'join' error

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source "https://rubygems.org"
-ruby "1.9.3"
+ruby "2.1.1"
 
 gem 'sinatra'
 gem 'thin'
 gem 'pry'
+
+gem 'rack', '1.5.2'
--- main.rb
+++ main.rb
@@ -6,21 +6,49 @@ use Rack::Session::Cookie, :key => 'rack.session',
                            :path => '/',
                            :secret => 'I am not sure what this does'
 
+
 get '/' do
- erb :login
+	if session[:player_name]
+		redirect '/game'
+	else
+ 		redirect '/login'
+ 	end
+end
+
+get '/login' do 
+	erb :login
 end
 
 post '/login' do
-	erb :login
 	session[:player_name] = params['player_name']
-	redirect '/game'
+	session[:buy_in] = params['buy_in']
+	redirect '/bet'
 end
 
 get '/game' do
+	ranking = %w(2 3 4 5 6 7 8 9 10 J Q K)
+	suits = %w(♦ ♣ ♥ ♠)
+	deck = suits.product(ranking).shuffle!
+
+	session[:dealer_cards] = []
+	session[:player_cards] = []
+	session[:dealer_cards] << session[:deck].pop
+	session[:player_cards] << session[:deck].pop
+	session[:dealer_cards] << session[:deck].pop
+	session[:player_cards] << session[:deck].pop
 	erb :game
 end
 
 
 get '/profile' do
 	erb :'/users/profile'
+end
+
+get '/bet' do 
+	erb :bet
+end
+
+post '/bet' do 
+	session[:bet] = params['bet']
+	redirect '/game'
 end
